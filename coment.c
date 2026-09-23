2c6b51eaaa181441d813d587dbb16846fe989065
Gabriel_fsv <fcogabrieldossvieira@gmail.com>
2026-09-22 23:32:03 -0300

    Adiciona comentarios da analise

---
+++ coment.c
@@ -0,0 +1,5 @@
+// Sim. A comparação com a IA me ajudou a perceber formas diferentes de resolver os problemas.
+// Também consegui identificar erros no meu código que antes não tinha percebido.
+// Percebi que uma solução gerada por IA também precisa ser analisada e testada.
+// A atividade me ajudou a entender melhor o uso de operações bit a bit e da janela deslizante.
+// Além disso, passei a prestar mais atenção às condições e à inicialização das variáveis.
